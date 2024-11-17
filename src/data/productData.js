b90faba9d82353f2883b937529e2aaf50836160b
clintzy <clielizek.joy@gmail.com>
2024-11-17 22:00:12 +0800

    added initial sample data

---
+++ src/data/productData.js
@@ -0,0 +1,66 @@
+const cookies = [
+  {
+    id: 1,
+    name: "Combo Box",
+    img_src:
+      "https://static.wixstatic.com/media/c837a6_b84460d516464a4a9e3b53bc9a4025f0~mv2.jpeg/v1/fill/w_908,h_908,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_b84460d516464a4a9e3b53bc9a4025f0~mv2.jpeg",
+    price: 32,
+    categories: ["combo_boxes", "bestseller"],
+  },
+  {
+    id: 2,
+    name: "The Dream Combo",
+    img_src:
+      "https://static.wixstatic.com/media/c837a6_a908d90c8dee4f94901bdd081a885101~mv2.jpeg/v1/fill/w_908,h_908,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_a908d90c8dee4f94901bdd081a885101~mv2.jpeg",
+    price: 32,
+    categories: ["combo_boxes", "bestseller"],
+  },
+  {
+    id: 3,
+    name: "Red Velvet",
+    img_src:
+      "https://static.wixstatic.com/media/c837a6_1747c76e203a47bf81ed96da6fe6d9ef~mv2.jpg/v1/fill/w_908,h_908,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_1747c76e203a47bf81ed96da6fe6d9ef~mv2.jpg",
+    price: 32,
+    categories: ["bestseller"],
+  },
+  {
+    id: 4,
+    name: "Lemon Meringue",
+    img_src:
+      "https://static.wixstatic.com/media/c837a6_79bed828b1dd47bda3fa43cdc2920f0d~mv2.jpg/v1/fill/w_908,h_908,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_79bed828b1dd47bda3fa43cdc2920f0d~mv2.jpg",
+    price: 32,
+    categories: ["bestseller"],
+  },
+  {
+    id: 6,
+    name: "Pistachio Mirage",
+    img_src:
+      "https://static.wixstatic.com/media/c837a6_00e3407ad707421e91f915620bb3699d~mv2.jpeg/v1/fill/w_908,h_908,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_00e3407ad707421e91f915620bb3699d~mv2.jpeg",
+    price: 32,
+    categories: ["bestseller"],
+  },
+  {
+    id: 7,
+    name: "Macadamia Heaven",
+    img_src:
+      "https://static.wixstatic.com/media/c837a6_ea9e317261f5488b89dcf745a62149b3~mv2.jpg/v1/fill/w_908,h_908,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_ea9e317261f5488b89dcf745a62149b3~mv2.jpg",
+    price: 32,
+    categories: ["bestseller"],
+  },
+  {
+    id: 9,
+    name: "Upside Downs",
+    img_src:
+      "https://static.wixstatic.com/media/c837a6_d6d61c9852af4b84ad1b2d92c9556076~mv2.jpeg/v1/fill/w_908,h_908,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_d6d61c9852af4b84ad1b2d92c9556076~mv2.jpeg",
+    price: 32,
+    categories: ["bestseller"],
+  },
+  {
+    id: 10,
+    name: "The Classics",
+    img_src:
+      "https://static.wixstatic.com/media/c837a6_010a6939ff924576815c13650ba50559~mv2.jpeg/v1/fill/w_908,h_908,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_010a6939ff924576815c13650ba50559~mv2.jpeg",
+    price: 32,
+    categories: ["bestseller"],
+  },
+];
