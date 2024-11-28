564fc2ceed4472b48d0f714e9caf73cdfe42f802
clintzy <clielizek.joy@gmail.com>
2024-11-28 21:26:14 +0800

    staggerChildren effects in Gallery

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,47 +1,68 @@
 import React from "react";
 import { FaPesoSign } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import ReviewStars from "../reviews/ReviewStars";
 
-
 export default function Gallery({ products, bgColor, category }) {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2, // Stagger children animation by 0.2s
+        delayChildren: 0.2,  // Delay the start of the children animation
+      },
+    },
+  };
 
-// bg-[#cfad96]
+  const itemVariants = {
+    hidden: { opacity: 0, y: -20 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
+
   return (
-    <div className=" p-5 pb-20   ">
-      <h1 className=" text-customBrown-darkest text-2xl mb-5 font-bold">{category}</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-center items-center ">
+    <motion.div
+      className="p-5 pb-20"
+      initial="hidden"
+      animate="show"
+      variants={containerVariants}
+    >
+      <h1 className="text-customBrown-darkest text-2xl mb-5 font-bold">
+        {category}
+      </h1>
+      <motion.div
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-center items-center"
+        variants={containerVariants}
+      >
         {products.map((product) => (
-          <div
+          <motion.div
             key={product.id}
-            className={` overflow-hidden grid relative  mx-auto `}
+            className="overflow-hidden grid relative mx-auto"
+            variants={itemVariants}
           >
-            <div className=" overflow-hidden  rounded-t ">
+            <div className="overflow-hidden rounded-t">
               <Link to={`/products/${product.name}`} state={product}>
-              <img
-                src={product.img_src}
-                alt={product.name}
-                className="w-full h-full hover:scale-125 duration-300 cursor-pointer  aspect-square"
-              />
+                <img
+                  src={product.img_src}
+                  alt={product.name}
+                  className="w-full h-full hover:scale-125 duration-300 cursor-pointer aspect-square"
+                />
               </Link>
             </div>
-            <div className=" border border-customBrown-light  p-2 rounded-b">
-              <p className="text-customBrown-darkest font-thin line-clamp-1 ">{product.name}</p>
+            <div className="border border-customBrown-light p-2 rounded-b">
+              <p className="text-customBrown-darkest font-thin line-clamp-1">
+                {product.name}
+              </p>
               <ReviewStars cookie={product} />
               <p className="text-customBrown-dark flex space-x-2 items-center font-light mt-2">
                 <FaPesoSign />
-              {product.price.toFixed(2)}</p>
+                {product.price.toFixed(2)}
+              </p>
             </div>
-            {/* <span
-              className={`${
-                product.on_sale ? "flex" : "hidden"
-              } ml-5 text-black border-black border px-2 rounded absolute top-2`}
-            >
-              Sale
-            </span> */}
-          </div>
+          </motion.div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
